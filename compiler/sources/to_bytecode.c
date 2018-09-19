b4c0ab0505585162a5fd73f3d24c6cc6fe2987b9
Oleksandr LEVCHENKOV <ollevche@student.unit.ua>
2018-09-19 19:11:25 +0300

    completed to_bytecode (but not tested)

--- compiler/sources/to_bytecode.c
+++ compiler/sources/to_bytecode.c
@@ -28,7 +28,7 @@ static int	get_size(t_item *item)
 	size = OP_CODE_SIZE;
 	size += g_optab[TYPE].codage;
 	i = 0;
-	while (i < 3 || ARG_TYPES[i])
+	while (i < 3 && ARG_TYPES[i])
 	{
 		if (ARG_TYPES[i] == REG_CODE)
 			size += 1;
@@ -51,7 +51,7 @@ static void	set_codage(t_item *item)
 		return ;
 	codage = 0;
 	i = 0;
-	while (i < 3 || ARG_TYPES[i])
+	while (i < 3 && ARG_TYPES[i])
 	{
 		type = ARG_TYPES[i];
 		codage |= type << (6 - i * 2); // check it
@@ -60,11 +60,42 @@ static void	set_codage(t_item *item)
 	CODE[1] = codage;
 }
 
+static int	write_arg(int type, int value, t_uchar *code) // check it // nums are unsigned
+{
+	if (type == REG_CODE)
+	{
+		code[0] = value & 255;
+		return (1);
+	}
+	if (type == IND_CODE || (type == DIR_CODE && !g_optab[TYPE].label_size))
+	{
+		code[1] = value & 255;
+		code[0] = value & 65280;
+		return (2);
+	}
+	if (type == DIR_CODE && g_optab[TYPE].label_size)
+	{
+		code[3] = value & 255;
+		code[2] = value & 65280;
+		code[1] = value & 16711680;
+		code[0] = value & 4278190080;
+		return (4);
+	}
+	return (0);
+}
+
 static void	set_args(t_item *item)
 {
 	int	pos;
+	int	i;
 
 	pos = OP_CODE_SIZE + g_optab[TYPE].codage;
+	i = 0;
+	while (i < 3 && ARG_TYPES[i])
+	{
+		pos += write_arg(ARG_TYPES[i], ARG_VALUES[i], CODE + pos);
+		i++;
+	}
 }
 
 bool		to_bytecode(t_item *head)
